8e58e600c1c7c319bb78af4e371e4cc8761a4559
PJCB1998 <pjcb1998@hotmail.com>
2023-08-09 10:44:01 -0500

    fix

--- users-api-launcher.sh
+++ users-api-launcher.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash 
 
-JWT_SECRET=PRFT SERVER_PORT=8083 java -jar /app/users-api/target/users-api-0.0.1-SNAPSHOT.jar &
+JWT_SECRET=PRFT SERVER_PORT=8083 /usr/bin/java -jar /app/users-api/target/users-api-0.0.1-SNAPSHOT.jar &
