9b6d55aa4dbf4d55b5751e0907a415833dfdfb12
Pablo Calle <pjcb1998@hotmail.com>
2023-08-10 14:50:59 -0500

    Update users-api-launcher.sh

--- users-api-launcher.sh
+++ users-api-launcher.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash 
 
-JWT_SECRET=PRFT SERVER_PORT=8083 SPRING_ZIPKIN_BASE_URL=http://192.168.56.80:9411 /usr/bin/java -jar /app/users-api/target/users-api-0.0.1-SNAPSHOT.jar > /var/log/user-api-launcher_log
+JWT_SECRET=PRFT SERVER_PORT=8083 SPRING_ZIPKIN_BASE_URL=http://192.168.56.80:9411/ /usr/bin/java -jar /app/users-api/target/users-api-0.0.1-SNAPSHOT.jar > /var/log/user-api-launcher_log
